202bf11cfb41fdccbdf1656f322f3d42b2d40476
Gustavo Suto <guyrux@gmail.com>
2022-05-15 22:12:56 -0300

    Testando erro - parte 2.

--- src/data/brasil_io_json.py
+++ src/data/brasil_io_json.py
@@ -1,15 +1,27 @@
 # %%
-
-import json
 import pandas as pd
 
-caminho = '../../data/raw/base_brasilio.json'
 
-temp = pd.read_json(caminho)
-df = pd.json_normalize(temp["results"])
+caminho = './data/raw/base_brasilio.json'
+
+
+def read_json(path: str = caminho) -> pd.DataFrame:
+    """Reads a json file and returns a pd.Dataframe.
+
+    Args:
+        path (str, optional): _description_. Defaults to caminho.
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    temp = pd.read_json(path)
+    return pd.json_normalize(temp["results"])
+
 
 # with open(caminho, 'r', encoding='utf-8') as f:
-#     lines = json.load(f)
 
+#     lines = json.load(f) 
 
-# %%
+if __name__ == '__main__':
+    caminho_main = '../../data/raw/base_brasilio.json'
+    df = read_json(caminho_main)
---
+++ src/data/create_views.py
@@ -0,0 +1,16 @@
+# %%
+# from src.data.brasil_io_json import read_json
+# from src.data.brasil_io import BrasilIO
+
+
+def eh_a_main():
+    if __name__ == '__main__':
+        print("É a main.")
+    else:
+        print("Não é a main")
+
+
+if __name__ == '__main__':
+    eh_a_main()
+
+# %%
