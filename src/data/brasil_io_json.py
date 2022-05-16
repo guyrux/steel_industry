5a428b64632e373d5980abde28d31a14c5a68e48
marcodeamicis <marcodeamicis@gmail.com>
2022-05-15 21:33:40 -0300

    WIP: Creating loading file

---
+++ src/data/brasil_io_json.py
@@ -0,0 +1,15 @@
+# %%
+
+import json
+import pandas as pd
+
+caminho = '../../data/raw/base_brasilio.json'
+
+temp = pd.read_json(caminho)
+df = pd.json_normalize(temp["results"])
+
+# with open(caminho, 'r', encoding='utf-8') as f:
+#     lines = json.load(f)
+
+
+# %%
